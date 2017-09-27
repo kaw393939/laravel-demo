ef0219279c81f3ab6c4bcc17b68805825268ff93
Keith Williams <keith@webizly.com>
2017-09-27 19:16:09 -0400

    laravel is great

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel is great
                 </div>
 
                 <div class="links">
